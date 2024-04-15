c612b65691635667a4c595c7b0e02579b21276f3
sajibalam8 <sajibalam8@hotmail.com>
2024-04-15 11:10:47 -0400

    SA Initial Commit for CodeHeaderCommentGenerator

---
+++ CodeHeaderCommentGeneratorPackage.cs
@@ -0,0 +1,21 @@
+﻿global using System;
+global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace CodeHeaderCommentGenerator
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.CodeHeaderCommentGeneratorString)]
+    public sealed class CodeHeaderCommentGeneratorPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
